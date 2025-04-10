b8b10ad654280f0984fec6a3107c9a4fad264370
Abdellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-04-10 15:27:17 +0100

    here i make the route of the update in the right whyt

--- resources/views/Dashebored_categories.blade.php
+++ resources/views/Dashebored_categories.blade.php
@@ -360,8 +360,6 @@
                                         </div>
                                     <!-- Pagination -->
                                     {{ $categories->links() }}
-
-
                                     <!-- Create Category Form Modal -->
                                     <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="categoryFormModal">
                                         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
